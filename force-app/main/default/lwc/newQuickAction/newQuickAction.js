dc41e0aff300cb0576b184ff85a268dfc1cdd39a
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-10-20 09:53:48 +0530

    quick action lwc component added

---
+++ force-app/main/default/lwc/newQuickAction/newQuickAction.js
@@ -0,0 +1,33 @@
+import { LightningElement, api } from 'lwc';
+import { getRecord, updateRecord } from 'lightning/uiRecordApi';
+import updateDurationField from '@salesforce/apex/updateFieldDuration.updateDurationField';
+
+
+export default class NewQuickAction extends LightningElement {
+    startTime;
+    endTime;
+    openTime;
+    @api recordId;
+
+    connectedCallback() {
+        this.startTime = new Date();
+        console.log('this.startTime', this.startTime);
+    }
+
+    disconnectedCallback() {
+        this.endTime = new Date();
+        this.openTime = (this.endTime - this.startTime) / 1000;
+
+        console.log('this.openTime', this.openTime);
+        console.log('this.recordId----->', this.recordId);
+
+        updateDurationField({ accountId: this.recordId, openTime: this.openTime })
+            .then(result => {
+                console.log('updated', result);
+                
+            })
+            .catch(error => {
+                console.error('Error---->', error);
+            });
+    }   
+}
